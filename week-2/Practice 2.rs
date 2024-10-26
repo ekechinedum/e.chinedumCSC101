65d9516046bb8f86d62a7083e26a2eaca59f5d1a
7nedum.eke@gmail.com <7nedum.eke@gmail.com>
2024-10-26 23:49:41 +0100

    (Week 2 commit)

---
+++ week-2/Practice 2.rs
@@ -0,0 +1,12 @@
+fn main(){
+
+    println!();
+
+    println!("hello ");
+
+    println!("format () arguments", "some");
+
+    println!("My name is (), I am the class rep of stream 1","Eke Chinedum");
+
+    
+          }
---
+++ week-2/Project 2.rs
@@ -0,0 +1,15 @@
+fn main(){
+   let T = 2*450_000;
+   let M = 1*1_500_000;
+   let H = 3*750_000;
+   let D = 3*2_850_000;
+   let A = 1*250_000;
+ let S = T+M+H+D+A
+ let Av = (S/5);
+ println!("The Sum total is (), and the average amount is () ", S,Av);
+
+
+
+
+
+}
