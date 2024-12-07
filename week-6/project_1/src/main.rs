f0b3b45d6cef9cfaced50ddc0f78722838b21859
7nedum.eke@gmail.com <7nedum.eke@gmail.com>
2024-12-07 11:42:18 +0000

    (Week-6)

---
+++ week-6/project_1/src/main.rs
@@ -0,0 +1,45 @@
+use stdin::io
+
+fn main() {
+
+fn trape(){
+
+    println!("Input height");
+    let mut height = String::new();
+    io::stdin().read_line(&mut height).expect("Failed to read");
+    let height:i64 = height.trim().parse().expect("Failed to input");
+
+    println!("Input base1");
+    let mut base1 = String::new();
+    io::stdin().read_line(&mut base1).expect("Failed to read");
+    let base1:i64 = base1.trim().parse().expect("Failed to input");
+
+    println!("Input base2");
+    let mut base2 = String::new();
+    io::stdin().read_line(&mut base2).expect("Failed to read");
+    let base2:i64 = base2.trim().parse().expect("Failed to input");
+    
+    }
+fn rhombus() {
+    println!("input daiognal1");
+    let mut diagonal = String::new();
+    io::stdin().read_line(&mut height).expect("failed) to read");
+    let diagonal1:i64 = diagonal1.trim().parse().expect("Failed to input");
+
+    println!("input diagonal2");
+    let mut diagonal2 = String::new();
+    io::stdin().read_line(&mut diagonal2).expect("Failed to read");
+    let diagonal2:i64 = diagonal2.trim().parse().expect("Failed to input");
+
+    }
+fn parallelogram() {
+    println!("input base");
+    let mut base = String::new();
+    io::stdin().read_line(&mut base).expect("Failed to read");
+    let basee:i64 = base.trim().parse().expect("Failed to input");
+        }
+
+
+fn 
+
+    }
